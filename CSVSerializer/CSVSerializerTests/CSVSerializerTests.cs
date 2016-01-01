5381345ffe43982d27aede141fe4a574586d71f4
douglasg14b <douglasg14b@gmail.com>
2015-12-31 17:24:42 -0800

    Created a few basic UnitTests. All Passing.

---
+++ CSVSerializer/CSVSerializerTests/CSVSerializerTests.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSVSerialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVSerialization.Tests
+{
+    [TestClass()]
+    public class CSVSerializerTests
+    {
+        [TestMethod()]
+        public void GetCSVString_PrimitiveTest()
+        {
+            TestData testData = new TestData();
+
+            List<TestData.TestPrimitiveData> dataList = new List<TestData.TestPrimitiveData>()
+            {
+                new TestData.TestPrimitiveData()
+            };
+
+            CSVSerializer<TestData.TestPrimitiveData> serializer = new CSVSerializer<TestData.TestPrimitiveData>();
+
+            string output = serializer.GetCSVString(dataList);
+
+            Assert.AreEqual(output, testData.expectedPrimitiveOutput);
+        }
+
+        [TestMethod()]
+        public void GetCSVString_ComplexTest()
+        {
+            TestData testData = new TestData();
+
+            List<TestData.TestComplexData> dataList = new List<TestData.TestComplexData>()
+            {
+                new TestData.TestComplexData()
+            };
+
+            CSVSerializer<TestData.TestComplexData> serializer = new CSVSerializer<TestData.TestComplexData>();
+
+            string output = serializer.GetCSVString(dataList);
+            serializer.WriteCSV("TestOutput.csv", dataList);
+
+            Assert.AreEqual(output, testData.expectedComplexOutput);
+        }
+
+        [TestMethod()]
+        public void GetCSVString_ModifiedHeadersTest()
+        {
+            TestData testData = new TestData();
+
+            List<TestData.TestComplexData> dataList = new List<TestData.TestComplexData>()
+            {
+                new TestData.TestComplexData()
+            };
+
+            List<string> columnNames = new List<string>()
+            {
+                "Test String",
+                "Test Complex String"
+            };
+
+            CSVSerializer<TestData.TestComplexData> serializer = new CSVSerializer<TestData.TestComplexData>();
+
+            string output = serializer.GetCSVString(dataList, columnNames);
+            string expectedOutput = "Test String,Test Complex String\nTest String...,\"Test \"\" Complex ;;;;;  string\"\"\\\\  \b    \"\n";
+
+            Assert.AreEqual(output, expectedOutput);
+        }
+
+        [TestMethod()]
+        public void GetCSVString_CustomHeadersTest()
+        {
+            TestData testData = new TestData();
+
+            List<TestData.TestComplexData> dataList = new List<TestData.TestComplexData>()
+            {
+                new TestData.TestComplexData()
+            };
+
+            List<CustomHeader> customHeaders = new List<CustomHeader>()
+            {
+                new CustomHeader("TestString", "Test String Header"),
+                new CustomHeader("ListIntTest", "List Int Test Header"),
+                new CustomHeader("TestComplexString", "Test Complex String Header")
+            };
+
+            CSVSerializer<TestData.TestComplexData> serializer = new CSVSerializer<TestData.TestComplexData>();
+
+            string output = serializer.GetCSVString(dataList, customHeaders);
+            string expectedOutput = "Test String Header,List Int Test Header,Test Complex String Header\nTest String...,\"1, 2, 3, 4, 5, 6, 7, 8, 9, 10\",\"Test \"\" Complex ;;;;;  string\"\"\\\\  \b    \"\n";
+
+            Assert.AreEqual(output, expectedOutput);
+        }
+
+        [TestMethod()] //Tests including two non-supported data types through custom headers
+        public void GetCSVString_CustomHeaders_IncludeTest()
+        {
+            TestData testData = new TestData();
+
+            List<TestData.TestComplexData> dataList = new List<TestData.TestComplexData>()
+            {
+                new TestData.TestComplexData()
+            };
+
+            //Includes a non-supported data type and excludes a supported one
+            List<CustomHeader> customHeaders = new List<CustomHeader>()
+            {
+                new CustomHeader("TestString", "Test String Header"),
+                new CustomHeader("Structure", "Structure Header"), //Not supported
+                new CustomHeader("ListIntTest", "List Int Test Header"),
+                new CustomHeader("DateTimeTest", "Date Time Test Header"),
+                new CustomHeader("TestComplexString", "Test Complex String Header")
+            };
+
+            CSVSerializer<TestData.TestComplexData> serializer = new CSVSerializer<TestData.TestComplexData>();
+
+            string output = serializer.GetCSVString(dataList, customHeaders);
+            string expectedOutput = "Test String Header,Structure Header,List Int Test Header,Date Time Test Header,Test Complex String Header\nTest String...,CSVSerialization.Tests.TestData+TestStruct,\"1, 2, 3, 4, 5, 6, 7, 8, 9, 10\",11/22/2015 12:00:00 AM,\"Test \"\" Complex ;;;;;  string\"\"\\\\  \b    \"\n";
+
+            Assert.AreEqual(output, expectedOutput);
+        }
+
+        [TestMethod()] //Tests custom headers excluding a supported type
+        public void GetCSVString_CustomHeaders_ExcludeTest()
+        {
+            TestData testData = new TestData();
+
+            List<TestData.TestComplexData> dataList = new List<TestData.TestComplexData>()
+            {
+                new TestData.TestComplexData()
+            };
+
+            List<CustomHeader> customHeaders = new List<CustomHeader>()
+            {
+                new CustomHeader("TestString", "Test String Header"),
+                new CustomHeader("TestComplexString", "Test Complex String Header")
+            };
+
+            CSVSerializer<TestData.TestComplexData> serializer = new CSVSerializer<TestData.TestComplexData>();
+
+            string output = serializer.GetCSVString(dataList, customHeaders);
+            string expectedOutput = "Test String Header,Test Complex String Header\nTest String...,\"Test \"\" Complex ;;;;;  string\"\"\\\\  \b    \"\n";
+
+            Assert.AreEqual(output, expectedOutput);
+        }
+    }
+}
---
+++ CSVSerializer/CSVSerializerTests/TestData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/**************************************************
+    A known good dataset in which to test
+ **************************************************/
+namespace CSVSerialization.Tests
+{
+    public class TestData
+    {
+
+        public string expectedPrimitiveOutput = "TestBool,TestShort,TestUShort,TestInt,TestUInt,TestLong,TestULong,TestFloat,TestDouble\nTrue,32767,65535,2147483647,4294967295,9223372036854775807,18446744073709551615,3.402823E+38,1.79769313486232E+308\n";
+        public string expectedComplexOutput = "TestString,TestComplexString,ListIntTest\nTest String...,\"Test \"\" Complex ;;;;;  string\"\"\\\\  \b    \",\"1, 2, 3, 4, 5, 6, 7, 8, 9, 10\"\n";
+        public class TestPrimitiveData
+        {
+            public bool TestBool { get; set; } = true;
+
+            public short TestShort { get; set; } = short.MaxValue;
+            public ushort TestUShort { get; set; } = ushort.MaxValue;
+            public int TestInt { get; set; } = int.MaxValue;
+            public uint TestUInt { get; set; } = uint.MaxValue;
+            public long TestLong { get; set; } = long.MaxValue;
+            public ulong TestULong { get; set; } = ulong.MaxValue;
+
+            public float TestFloat { get; set; } = float.MaxValue;
+            public double TestDouble { get; set; } = double.MaxValue;
+            public decimal TestDecimal { get; set; } = decimal.MaxValue;
+        }
+
+        public class TestComplexData
+        {
+            public DateTime DateTimeTest { get; set; } = new DateTime(2015, 11, 22);
+            public string TestString { get; set; } = "Test String...";
+            public string TestComplexString { get; set; } = "Test \" Complex ;;;;; \n string\"\\\\  \b  \n  ";
+            public TestStruct Structure { get; set; } = new TestStruct();
+            public List<int> ListIntTest { get; set; } = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        }
+
+        public struct TestStruct
+        {
+            public short TestShort { get { return short.MaxValue; }}
+        }
+    }
+}
